08b4eb9004d0c4facc9d4ccb06abce409be608bd
OlgaFedorova <olga.fedorova.poluhina@gmail.com>
2018-11-10 18:26:35 +0300

    configs for docker profile

---
+++ zipkin-server/src/main/java/ofedorova/zipkinserver/ZipkinServerApplication.java
@@ -0,0 +1,16 @@
+package ofedorova.zipkinserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+
+@SpringBootApplication
+@EnableZipkinStreamServer
+@EnableEurekaClient
+public class ZipkinServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinServerApplication.class, args);
+	}
+}
