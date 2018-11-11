efebf1969562a48bd88b142ab74637a003782555
OlgaFedorova <olga.fedorova.poluhina@gmail.com>
2018-11-11 20:03:10 +0300

    change version

---
+++ airline-planes/src/main/java/ofedorova/airlineplanes/AirlinePlanesApplication.java
@@ -0,0 +1,12 @@
+package ofedorova.airlineplanes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlinePlanesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirlinePlanesApplication.class, args);
+	}
+}
